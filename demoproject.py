690800291411c8c3d7f999fbca2fc6d946fe6c54
vikas <vikas@gmail.com>
2024-09-22 17:35:03 +0530

    v2

--- demoproject.py
+++ demoproject.py
@@ -1,3 +1,19 @@
 # Today we will learn git branch
 def fun(x,y):
     return x + y
+
+num = fun(5,2)
+print(num)
+
+def prime(num):
+    if num <= 0:
+        return '0 or -ve numbers are not prime'
+    elif num <= 3:
+        return 'Prime'
+    else:
+        for i in range(2, num):
+            if num % i == 0:
+                return 'Not Prime'
+    return 'Prime'
+        
+print(prime(num))
